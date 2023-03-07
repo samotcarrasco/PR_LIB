720c9b114bc1a73e38e24e2e8b9d8478cf4564df
samot <samotcarrasco@gmail.com>
2023-03-07 12:44:18 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/HelloWorldLib.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+public class HelloWorldLib {
+
+	public static void main(String[] args) {
+	}
+	
+	public static String prueba () {
+		return "hola";
+	}
+
+	public static void prueba2 () {
+		System.out.println("hola");
+	}
+
+}
